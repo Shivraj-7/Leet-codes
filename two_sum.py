2ac323bd99d21605511568258d191d18af6e72cd
Shivraj Ambhore <shivrajambhore777@gmail.com>
2025-02-11 15:16:42 +0530

    two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,6 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
-       for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# Example usage
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    solution = Solution()
+    result = solution.twoSum(nums, target)
+    print(result)  # Output: [0, 1]
